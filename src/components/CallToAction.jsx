7a2e94ceb92a2e83b9db430677934e02fa9d7330
Even Wei <evenwei@infuseai.io>
2023-06-04 13:54:52 +0800

    Emphasize open source and imporve CTA
    Signed-off-by: Even Wei <evenwei@infuseai.io>

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -26,21 +26,21 @@ export function CallToAction() {
       <Container className="relative">
         <div className="mx-auto max-w-lg text-center">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
-            Get started today
+            Next Steps
           </h2>
           <p className="mt-4 text-lg tracking-tight text-white">
-            It’s time to take control of your books. Buy our software so you can
-            feel like you’re doing something productive.
+            Try PipeRider today and stop worrying about breaking prod.
+            PipeRdier Cloud is free to get started, and offers flexibility for teams of different sizes and budgets.
           </p>
           <Button
             href="https://cloud.piperider.io/signup?ref=prio-cta"
             color="white"
             className="mt-10"
             onClick={() => {
-              track("[Action] Click CTA Launch Cloud");
+              track("[Action] Click CTA Cloud");
             }}
           >
-            Launch Cloud
+            Try PipeRider Cloud
           </Button>
         </div>
       </Container>
