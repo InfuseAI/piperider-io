843a1671624cf68a7bf9a3b574af685b873cc3eb
Kent Huang <kent@infuseai.io>
2023-07-05 14:49:37 +0800

    [Request] Add discord redirect page
     - https://piperider.io/discord -> InfuseAI discord invite page
    
    Signed-off-by: Kent Huang <kent@infuseai.io>

---
+++ src/pages/discord.jsx
@@ -0,0 +1,12 @@
+// pages/redirect
+
+import {useEffect} from 'react'
+export default function redirect() {
+    useEffect(() => {
+        window.location.assign('https://discord.gg/b3HAykQPkn')
+    })
+    return(
+        <>
+        </>
+    )
+}
