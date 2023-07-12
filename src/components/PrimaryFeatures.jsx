c7012489519378a398643c6443687076ae5d6e3c
H <hanleyweng@H-MBP-InfuseAI.local>
2023-07-12 15:28:06 +1000

    Primary Features Copy
    Hello World test. Bandaid fix on Primary Features copy.

--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -66,11 +66,10 @@ export function PrimaryFeatures() {
       <Container className="relative">
         <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
-            Everything you need to run your books.
+            Enhanced Data Quality at Your Fingertips
           </h2>
           <p className="mt-6 text-lg tracking-tight text-blue-100">
-            Well everything you need if you aren’t that picky about minor
-            details like tax compliance.
+            Empower your data engineering processes with robust and accurate information.
           </p>
         </div>
         <Tab.Group
