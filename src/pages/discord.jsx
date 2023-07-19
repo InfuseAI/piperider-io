3dc4c14e4ce77a82dfe62b7d97d4798ba2d72f39
H <hanleyweng@H-MBP-InfuseAI.local>
2023-07-19 10:54:10 +1000

    Updated Comments about Discord redirect Error message

--- src/pages/discord.jsx
+++ src/pages/discord.jsx
@@ -3,6 +3,13 @@
 // import {useEffect} from 'react'
 
 export default function redirect() {
+    /*
+        // After ChakraUI Addition: Error:
+            ./src/pages/discord.jsx
+            5:5  Error: React Hook "useEffect" is called in function "redirect" that is neither a React function component nor a custom React Hook function. React component names must start with an uppercase letter. React Hook names must start with the word "use".  react-hooks/rules-of-hooks
+
+        // TODO: Reimplement.
+    */
     // useEffect(() => {
     //     window.location.assign('https://discord.gg/b3HAykQPkn')
     // })
