2a7012fe31008b3b927c965318573c7d3a5e8fba
H <hanleyweng@H-MBP-InfuseAI.local>
2023-07-19 12:39:59 +1000

    Added Modal - for Image Zoom
    This serves as a ChakraUI Modal Hello World - to test for any conflicts with previous components / packages.

--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -8,6 +8,12 @@ import backgroundImage from "@/images/background-features.jpg";
 
 import { track } from "@amplitude/analytics-browser";
 
+import { Text, Icon, Spacer, Button } from '@chakra-ui/react'
+import { useDisclosure } from '@chakra-ui/react'
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from '@chakra-ui/react'
+
+/////////////////////////////////////////////////////////////
+
 const features = [
   {
     title: "Know Thy Data",
@@ -31,6 +37,11 @@ const features = [
 
 export function PrimaryFeatures() {
   let [tabOrientation, setTabOrientation] = useState("horizontal");
+  
+  let [zoomedImage, setZoomedImage] = useState(null);
+  let [zoomedImageTitle, setZoomedImageTitle] = useState(null);
+
+  const imageZoomModalDisclosure = useDisclosure();
 
   useEffect(() => {
     let lgMediaQuery = window.matchMedia("(min-width: 1024px)");
@@ -81,11 +92,19 @@ export function PrimaryFeatures() {
                   <p className="text-gray-200">{feature.description}</p>
                 </div>
               </div>
-              <div className={`flex-1 m-4 ${index % 2 != 0 ? 'md:order-first' : ''} hover:shadow-2xl transition-all duration-500 hover:-translate-y-2 hover:scale-105`}>
-                <a href={feature.image} target='_blank' rel='noopener noreferrer' className="cursor-zoom-in" 
+              <div 
+                className={`flex-1 m-4 ${index % 2 != 0 ? 'md:order-first' : ''} 
+                  hover:shadow-2xl transition-all duration-500 hover:-translate-y-2 hover:scale-105 cursor-pointer`}
                 onClick={() => {
+                  setZoomedImage(feature.image); 
+                  setZoomedImageTitle(feature.title);
                   track(`[Action] Zoom - ${index} - ${feature.title}`);
-                }}>
+                  imageZoomModalDisclosure.onOpen();
+                }}
+              >
+
+              {/* Todo: Readd Tracking OnClick */}
+
                   <Image
                     src={feature.image}
                     alt={feature.title}
@@ -94,13 +113,45 @@ export function PrimaryFeatures() {
                     width={500}
                     className='rounded-md'
                   />
-                </a>
               </div>
             </div>
           ))}
         </div>
 
       </Container>
+      <ImageZoomModal isOpen={imageZoomModalDisclosure.isOpen} onOpen={imageZoomModalDisclosure.onOpen} onClose={imageZoomModalDisclosure.onClose} imagePath={zoomedImage} imageTitle={zoomedImageTitle}  />
     </section>
   );
 }
+
+////////////////////////////////////////////////////////////
+
+export function ImageZoomModal( {isOpen, onOpen, onClose, imageTitle, imagePath}) {
+  return (
+    <Modal isOpen={isOpen} onOpen={onOpen} onClose={onClose} size="full">
+      <ModalOverlay />
+      <ModalContent m={8} borderRadius={16}>
+        <ModalHeader>{imageTitle ? imageTitle : 'Image'}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          {imagePath && (
+            <Image
+              src={imagePath}
+              layout="responsive"
+              height={500}
+              width={500}
+              className='rounded-md'
+            />
+          )}
+        </ModalBody>
+
+        <ModalFooter>
+          {/*<Button colorScheme='blue' mr={3} onClick={onClose}>
+            Close
+          </Button>*/}
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+
